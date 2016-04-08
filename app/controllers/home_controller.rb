055fa3784da9c4f0418e1db82044d8335f567a4b
Bas van Straaten <basvanstraaten@live.nl>
2016-04-08 22:23:56 +0200

    added contact form and analytics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,18 @@ class HomeController < ApplicationController
   def index
     @articles = Article.all.order(created_at: :DESC)
     # @projects = Project.all.order(created_at: :DESC)
+    @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+    else
+      flash.now[:error] = 'Cannot send message, please try again'
+      render :index
+    end
+  end
+
 end
